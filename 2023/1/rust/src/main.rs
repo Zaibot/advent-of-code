bb1541cdaf874a0661b65ce2d013d70d45486d76
Tobias de Groen <tobias@degroen.solutions>
2023-12-01 20:55:32 +0100

    day 1, input 2, working concept

--- 2023/1/rust/src/main.rs
+++ 2023/1/rust/src/main.rs
@@ -1,3 +1,6 @@
+use std::iter::Peekable;
+use std::str::Chars;
+
 fn main() {
     let total = std::fs::read_to_string("../input.txt")
         .expect("Unable to read file")
@@ -9,25 +12,115 @@ fn main() {
     println!("Total: {}", total);
 }
 
+const STR_NUMBER_MAP: [(&str, u8); 10] = [
+    ("zero", 0),
+    ("one", 1),
+    ("two", 2),
+    ("three", 3),
+    ("four", 4),
+    ("five", 5),
+    ("six", 6),
+    ("seven", 7),
+    ("eight", 8),
+    ("nine", 9),
+];
+
+enum Match {
+    None,
+    Partial,
+    Full(u8),
+}
+
+struct Matcher {
+    offset: usize,
+    state: Vec<Option<(&'static str, u8)>>,
+}
+
+impl Matcher {
+    pub fn new() -> Self {
+        Self {
+            offset: 0,
+            state: STR_NUMBER_MAP
+                .iter()
+                .map(|(s, n)| Some((*s, *n)))
+                .collect::<Vec<_>>(),
+        }
+    }
+
+    pub fn advance(&mut self, ch: char) -> Match {
+        let mut some = Match::None;
+
+        for state in self.state.iter_mut() {
+            if let Some(valid) = state {
+                if valid.0.as_bytes()[self.offset] == ch as u8 {
+                    if self.offset == valid.0.len() - 1 {
+                        some = Match::Full(valid.1);
+                        break;
+                    } else {
+                        some = Match::Partial;
+                    }
+                } else {
+                    *state = None;
+                }
+            }
+        }
+
+        self.offset += 1;
+        some
+    }
+}
+
+fn peek_str_number(iter: &mut Peekable<Chars>) -> Option<u8> {
+    println!("\npeek_str_number {:#?}", iter.clone().collect::<String>());
+    let mut peekahead = iter.clone();
+
+    let mut state = Matcher::new();
+    while let Some(ch) = peekahead.peek() {
+        print!("\n+{}", ch);
+        match state.advance(*ch) {
+            Match::None => {
+                println!("!");
+                return None;
+            }
+            Match::Partial => {
+                print!("?{}", state.state.iter().filter(|x| x.is_some()).count());
+            }
+            Match::Full(num) => {
+                println!("={}", num);
+                return Some(num);
+            }
+        }
+        peekahead.next();
+    }
+    None
+}
+
 fn extract_digit_first_last(text: &str) -> Option<(u8, u8)> {
     let mut first: Option<u8> = None;
     let mut last: Option<u8> = None;
 
-    let mut iter = text.chars();
+    let mut iter = text.chars().peekable();
 
-    while let Some(ch) = iter.next() {
+    while let Some(ch) = iter.peek() {
         if let Some(di) = ch.to_digit(10) {
             first = Some(di as u8);
             last = Some(di as u8);
             break;
-        }
-    }
-
-    while let Some(ch) = iter.next_back() {
-        if let Some(di) = ch.to_digit(10) {
-            last = Some(di as u8);
+        } else if let Some(num) = peek_str_number(&mut iter) {
+            first = Some(num);
+            last = Some(num);
             break;
         }
+        iter.next();
+    }
+
+    while let Some(ch) = iter.peek() {
+        if let Some(di) = ch.to_digit(10) {
+            last = Some(di as u8);
+        } else if let Some(num) = peek_str_number(&mut iter) {
+            last = Some(num);
+        }
+        iter.next();
     }
 
     matches!(first, Some(_)).then(|| (first.unwrap(), last.unwrap()))
@@ -52,7 +145,7 @@ fn test_example_input() {
         let first_last = extract_digit_first_last(input).unwrap();
         let combined = combine_digits(first_last).unwrap();
 
-        assert_eq!(combined, expected);
+        assert_eq!(combined, expected, "Input: {:#?}", input);
     }
 }
 
@@ -72,7 +165,7 @@ fn test_example_input_2() {
         let first_last = extract_digit_first_last(input).unwrap();
         let combined = combine_digits(first_last).unwrap();
 
-        assert_eq!(combined, expected);
+        assert_eq!(combined, expected, "Input: {:#?}", input);
     }
 }
 
@@ -89,5 +182,34 @@ fn test_combine_digits() {
     for (first, last, expected) in expected {
         let combined = combine_digits((first, last)).unwrap();
         assert_eq!(combined, expected);
+    }
+}
+
+#[test]
+fn test_peek() {
+    #[rustfmt::skip]
+    let expected = [
+        ("one", 1), 
+        (" one", 1), 
+        ("twone", 2), 
+        ("threone", 1),
+    ];
+
+    for (input, expected) in expected {
+        let parsed = parse_str(input);
+        assert_eq!(parsed, Some(expected), "Input: {:#?}", input);
+    }
+
+    fn parse_str(text: &str) -> Option<u8> {
+        println!("parse_str: {:#?}", text);
+        let mut iter = text.chars().peekable();
+        while iter.peek().is_some() {
+            if let Some(d) = peek_str_number(&mut iter) {
+                return Some(d);
+            }
+            iter.next();
+        }
+
+        None
     }
 }
